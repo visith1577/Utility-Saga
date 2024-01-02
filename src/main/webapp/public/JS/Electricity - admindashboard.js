8387d9835b50303118787f251fac2e657b47ee07
Nethmi Liyanage <nethmi2001liyanage@gmail.com>
2024-01-02 16:32:38 +0530

    regional admin-electricity

---
+++ src/main/webapp/public/JS/Electricity - admindashboard.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Get the canvas element
+    var ctx = document.getElementById('lineChart').getContext('2d');
+
+    // Define the data for the chart
+    var data = {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'Novemeber', 'December'],
+        datasets: [{
+            label: 'No. of new users',
+            data: [12, 19, 3, 5, 2, 3, 4, 5, 6, 7, 8, 9],
+            backgroundColor: 'rgb(220, 233, 0)',
+            borderColor: 'rgb(220, 233, 0)',
+            borderWidth: 1
+        }]
+    };
+
+    // Create the line chart
+    var lineChart = new Chart(ctx, {
+        type: 'line',
+        data: data,
+        options: {
+            responsive: true,
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+});
