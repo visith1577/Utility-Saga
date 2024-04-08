82ad628d1434def2db127368bdd8f64baa4c6b27
Nethmi Liyanage <nethmi2001liyanage@gmail.com>
2024-04-08 10:00:39 +0530

    Correct popup electricity regional admin (tab 1)

--- src/main/webapp/public/JS/Electricity - admindashboard.js
+++ src/main/webapp/public/JS/Electricity - admindashboard.js
@@ -28,3 +28,16 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+let popup = document.getElementById('popup');
+let popupcontainer = document.getElementById('popupcontainer');
+
+function openPopup() {
+      popup.classList.add("open-popup");
+      popupcontainer.classList.add("open-popupcontainer");
+}
+
+function closePopup() {
+      popup.classList.remove("open-popup");
+      popupcontainer.classList.remove("open-popupcontainer");
+}
