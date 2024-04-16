62dfd9aa7f9157beacd1894c629643d240122293
visith1577 <visithkumarapperuma@gmail.com>
2024-04-16 15:38:02 +0530

    solar search fix

--- src/main/webapp/public/JS/SolarSearch.js
+++ src/main/webapp/public/JS/SolarSearch.js
@@ -1,7 +1,7 @@
 function search() {
     var nic = document.getElementById("nic").value;
 
-    fetch('/UtilitySaga_war_exploded/solar/search?nic=' + nic)
+    fetch(contextPath + '/solar/search?nic=' + nic)
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
@@ -19,14 +19,14 @@ function search() {
 
 function displayResults(data) {
     console.log(data);
-    var resultsDiv = document.getElementById("results");
+    const resultsDiv = document.getElementById("results");
     resultsDiv.innerHTML = "";
     if (data.length === 0) {
         resultsDiv.innerHTML = "No results found.";
     } else {
-        var table = "<table class='table'><tr><th>Company Name</th><th>BRN</th><th>Owner-NIC</th><th>UserName</th><th>Password</th><th>Mobile</th><th>Company Phone</th><th>Email</th><th>Address</th><th>District</th><th>Remarks</th><th>Approved Status</th><th>Submit</th></tr>";
-        for (var i = 0; i < data.length; i++) {
-            table += "<tr><td>" + data[i].CompanyName +
+        let table = "<table class='table'><tr><th>Company Name</th><th>BRN</th><th>Owner-NIC</th><th>UserName</th><th>Password</th><th>Mobile</th><th>Company Phone</th><th>Email</th><th>Address</th><th>District</th><th>Remarks</th><th>Approved Status</th><th>Submit</th></tr>";
+        for (let i = 0; i < data.length; i++) {
+            table += `<tr data-bnum=${data[i].BNum}><td>` + data[i].CompanyName +
                 "</td><td>" + data[i].BNum +
                 "</td><td>" + data[i].OwnerNIC +
                 "</td><td>" + data[i].username +
