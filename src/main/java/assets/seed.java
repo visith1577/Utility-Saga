a895767a05dad3af7babc923a4b96d9ddad6daf8
visith1577 <visithkumarapperuma@gmail.com>
2024-04-26 17:54:08 +0530

    fix

--- src/main/java/assets/seed.java
+++ src/main/java/assets/seed.java
@@ -15,7 +15,7 @@ import org.mindrot.jbcrypt.BCrypt;
 import java.io.IOException;
 import java.sql.SQLException;
 
-@WebServlet("/initialize")
+@WebServlet("/user/test-field/initialize")
 public class seed extends HttpServlet {
 
     @Override
@@ -32,60 +32,60 @@ public class seed extends HttpServlet {
 //        String homePhone = "011 244 0316";
 //        String region = "headOffice";
 
-        String id = "Regional_Colombo";
-        String fname = "CEB";
-        String lname = "Regional";
-        String uname = "WB Admin Colombo";
-        String pwd = "rootAdmin";
-        String tel = "1987";
-        String email = "dgmp@ceb.lk";
-        String address = "23 Claessen Pl ";
-        String homePhone = "0114 498 498";
-        String region = "Colombo";
-        String empid = "WB001";
+//        String id = "Regional_Colombo";
+//        String fname = "CEB";
+//        String lname = "Regional";
+//        String uname = "WB Admin Colombo";
+//        String pwd = "rootAdmin";
+//        String tel = "1987";
+//        String email = "dgmp@ceb.lk";
+//        String address = "23 Claessen Pl ";
+//        String homePhone = "0114 498 498";
+//        String region = "Colombo";
+//        String empid = "WB001";
+////
+//        ElectricityAdminModel user = new ElectricityAdminModel();
+//        user.setId(id);
+//        user.setRegion(region);
+//        user.setContactNumber(tel);
+//        user.setEmail(email);
+//        user.setUtilityType(ElectricityAdminModel.UtilityType.valueOf("CEB"));
+//        user.setEmpId(empid);
+//        user.setUsername(uname);
+//        user.setFirstname(fname);
+//        user.setLastname(lname);
+//        user.setRole(ElectricityAdminModel.Role.valueOf("REGIONAL"));
+//        user.setMobile(homePhone);
+//        String bcryptHashedPwd = BCrypt.hashpw(pwd, BCrypt.gensalt());
+//        user.setPassword(bcryptHashedPwd);
 //
-        ElectricityAdminModel user = new ElectricityAdminModel();
-        user.setId(id);
-        user.setRegion(region);
-        user.setContactNumber(tel);
-        user.setEmail(email);
-        user.setUtilityType(ElectricityAdminModel.UtilityType.valueOf("CEB"));
-        user.setEmpId(empid);
-        user.setUsername(uname);
-        user.setFirstname(fname);
-        user.setLastname(lname);
-        user.setRole(ElectricityAdminModel.Role.valueOf("REGIONAL"));
-        user.setMobile(homePhone);
-        String bcryptHashedPwd = BCrypt.hashpw(pwd, BCrypt.gensalt());
-        user.setPassword(bcryptHashedPwd);
-
-
-        WaterAdminDAO dao = new WaterAdminDAO();
-
-        try {
-            dao.addWaterAdmin(user);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
-//        ReportGenerator report = new ReportGenerator();
-//        String report_text = report.dailyReport("23456444", 200, 180, 10_000);
 //
-//        resp.setContentType("text/html");
+//        WaterAdminDAO dao = new WaterAdminDAO();
 //
-//        // Write the report as response
-//        resp.getWriter().println(report_text);
-
-//        SendEmail sendEmail = new SendEmail(
-//                "2021cs105@stu.ucsc.cmb.ac.lk",
-//                new String[]{"visithkumarapperuma@gmail.com"},
-//                "Test Email",
-//                "This is a test email"
-//        );
-//        // Send the email
-//        sendEmail.sendEmail("<h1>Test Email --> UtilitySaga</h1>");
+//        try {
+//            dao.addWaterAdmin(user);
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
 //
-//        // Send a response to the client
-//        resp.getWriter().write("Email sent successfully");
+////        ReportGenerator report = new ReportGenerator();
+////        String report_text = report.dailyReport("23456444", 200, 180, 10_000);
+////
+////        resp.setContentType("text/html");
+////
+////        // Write the report as response
+////        resp.getWriter().println(report_text);
+//
+////        SendEmail sendEmail = new SendEmail(
+////                "2021cs105@stu.ucsc.cmb.ac.lk",
+////                new String[]{"visithkumarapperuma@gmail.com"},
+////                "Test Email",
+////                "This is a test email"
+////        );
+////        // Send the email
+////        sendEmail.sendEmail("<h1>Test Email --> UtilitySaga</h1>");
+////
+////        // Send a response to the client
+////        resp.getWriter().write("Email sent successfully");
     }
 }
